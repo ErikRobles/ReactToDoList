99287cccbe9b1f52a0cbe44efe76043dc51d50d1
ErikRobles <erikjames69@hotmail.com>
2019-01-25 23:18:33 -0600

    first Commit

---
+++ src/components/layout/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+function Header() {
+  return (
+    <div>
+      <header style={headerStyle} >
+          <h1>TodoList</h1>
+          <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
+      </header>
+    </div>
+  )
+}
+
+const headerStyle = {
+    background: '#333',
+    color: '#fff',
+    textAlign: 'center',
+    padding: '10px',
+    marginBottom: '15px'
+}
+
+const linkStyle = {
+  color: '#fff',
+  textDecoration: 'none'
+}
+
+export default Header;
