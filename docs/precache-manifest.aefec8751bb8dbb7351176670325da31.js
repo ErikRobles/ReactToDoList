8985446507930a7d7d28209f10c862dc73fbd3d4
ErikRobles <erikjames69@hotmail.com>
2019-01-28 10:16:04 -0600

    first real commit

---
+++ docs/precache-manifest.aefec8751bb8dbb7351176670325da31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "059c1d181915345b7445",
+    "url": "/ErikRobles/ReactToDoList.git/static/js/runtime~main.059c1d18.js"
+  },
+  {
+    "revision": "622359adbae758d4b47c",
+    "url": "/ErikRobles/ReactToDoList.git/static/js/main.622359ad.chunk.js"
+  },
+  {
+    "revision": "f5eb11596f52b644155e",
+    "url": "/ErikRobles/ReactToDoList.git/static/js/1.f5eb1159.chunk.js"
+  },
+  {
+    "revision": "622359adbae758d4b47c",
+    "url": "/ErikRobles/ReactToDoList.git/static/css/main.70a93bcf.chunk.css"
+  },
+  {
+    "revision": "bd4dcfdbd6fbcfa7d0aedcec0dcec78b",
+    "url": "/ErikRobles/ReactToDoList.git/index.html"
+  }
+];
